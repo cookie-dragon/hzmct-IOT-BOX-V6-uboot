240e6228d12f6e2b689f9c47b6826cd706807502
Cooky Long <lj12875@mail.haitian.com>
2021-03-09 17:55:44 +0800

    init 20171011

---
+++ am335/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Oct 11 2017"
+#define U_BOOT_TIME "09:43:20"
---
+++ build.sh
@@ -0,0 +1,2 @@
+make distclean CROSS_COMPILE=arm-linux-gnueabihf-
+make -j4 O=am335 CROSS_COMPILE=arm-linux-gnueabihf- am335x_evm 
